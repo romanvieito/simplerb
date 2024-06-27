7d7a7ba4629ef046d737118febaf863e265647c1
adony gonzalez cardenas <adonyglez850209@gmail.com>
2024-06-27 18:25:42 -0400

    update web generator with anthropic ia

---
+++ pages/api/anthropic.ts
@@ -0,0 +1,37 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import Anthropic from '@anthropic-ai/sdk';
+
+const API_TOKEN = process.env.ANTHROPIC_API_KEY ?? '';
+
+const anthropic = new Anthropic({
+  apiKey: API_TOKEN,
+});
+
+export default async function handler (
+    req: NextApiRequest,
+    res: NextApiResponse
+  ) {
+    if (req.method !== 'POST') {
+      return res.status(405).end('Method Not Allowed');
+    }
+
+    const { prompt } : { prompt: string } = req.body;    
+
+    if (!prompt) {
+      return new Response('No prompt in the request', { status: 400 });
+    } 
+    
+    try {
+      const message = await anthropic.messages.create({
+        model: 'claude-3-5-sonnet-20240620',
+        max_tokens: 1024,
+        messages: [
+          {"role": "user", "content": prompt }
+        ]
+      });
+
+      return res.status(200).json({ data: message });
+    } catch (error) {
+      return res.status(500).json({ error: error });
+    }    
+};
