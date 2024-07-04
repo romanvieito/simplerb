5671a9be3b6b5dbacc6a2e032e771be66238f045
adony gonzalez cardenas <adonyglez850209@gmail.com>
2024-07-04 08:55:00 -0400

    adding landing pages and update website page

--- pages/lpdomain.tsx
+++ pages/lpdomain.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 import Head from "next/head";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+import CFAQ from '../components/CFAQ';
 import CPricing from '../components/CPricing';
 
-export default function PricingPage() {
+export default function LqPage() {
 
   return (
     <div className="flex max-w-5xl mx-auto flex-col items-center justify-center py-2 min-h-screen">
@@ -12,9 +13,15 @@ export default function PricingPage() {
         <title>Domain Generator</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Header showPricing={false} showFAQ={true}/>
+      <Header/>
       <main className="flex flex-1 w-full flex-col items-center justify-center text-center px-4 mt-12 sm:mt-20">
-        <CPricing/>
+        <div>
+          <h2 className="mt-12 font-medium" style={{ fontSize: 30 }}>
+            Domain
+          </h2>
+          <CPricing/>
+          <CFAQ/>
+        </div>        
       </main>
       <Footer />
     </div>
