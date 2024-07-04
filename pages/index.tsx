5118a08dae00b2a369c5b444a3578ade5a9c7d72
adony gonzalez cardenas <adonyglez850209@gmail.com>
2024-07-04 09:11:05 -0400

    adding landing pages and update website page v3

--- pages/index.tsx
+++ pages/index.tsx
@@ -32,7 +32,7 @@ const Home: NextPage = () => {
         <title>Domain Generator</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Header showPricing={true} showFAQ={true}/>
+      <Header/>
       <main className="flex flex-1 w-full flex-col items-center justify-center text-center px-4 mt-12 sm:mt-20">
         <h1 className="sm:text-6xl text-4xl max-w-[708px] font-bold text-slate-900">
           Generate your business with AI
