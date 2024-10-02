90c0d5aee8d16ffe5016de0b3c32e1a289220ea1
Alberto Roman <romanvieito@gmail.com>
2024-10-02 12:34:12 -0400

    changing label

--- pages/domain.tsx
+++ pages/domain.tsx
@@ -574,7 +574,7 @@ const DomainPage: React.FC = () => {
                         onClick={() => setShowAdvancedSettings(false)}
                         className="px-4 py-2 bg-gray-500 text-white text-base font-medium rounded-md w-full shadow-sm hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-300"
                       >
-                        Close
+                        Done
                       </button>
                     </div>
                   </div>
