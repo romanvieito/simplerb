4ad14b5a3b36e461b68542aef037de7f6ef15777
Alberto Roman <romanvieito@gmail.com>
2025-03-19 05:56:31 -0400

    publish icon

--- pages/web.tsx
+++ pages/web.tsx
@@ -598,7 +598,7 @@ const WebPage = () => {
           ) : (
             <>
               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-1" viewBox="0 0 20 20" fill="currentColor">
-                <path fillRule="evenodd" d="M4 4v2h12V4H4zm0 3v2h12V7H4zm0 3v2h12v-2H4zm0 3v2h12v-2H4z" clipRule="evenodd" />
+                <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
               </svg>
               Publish
             </>
