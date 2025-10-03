b19d4390abd161ce59a5d0e44a0e066d00ab0ddd
Alberto Roman <romanvieito@gmail.com>
2025-10-03 12:44:48 -0400

    labels

--- pages/domain.tsx
+++ pages/domain.tsx
@@ -799,7 +799,7 @@ const DomainPage: React.FC = () => {
                                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 group-hover:scale-110 transition-transform" viewBox="0 0 20 20" fill="currentColor">
                                   <path d="M3 1a1 1 0 000 2h1.22l.305 1.222a.997.997 0 00.01.042l1.358 5.43-.893.892C3.74 11.846 4.632 14 6.414 14H15a1 1 0 000-2H6.414l1-1H14a1 1 0 00.894-.553l3-6A1 1 0 0017 3H6.28l-.31-1.243A1 1 0 005 1H3zM16 16.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM6.5 18a1.5 1.5 0 100-3 1.5 1.5 0 000 3z" />
                                 </svg>
-                                <span>{isSignedIn && isPremiumUser ? 'Buy Domain' : 'Check Availability'}</span>
+                                <span>{isSignedIn && isPremiumUser ? 'Buy' : 'Check Availability'}</span>
                               </button>
 
                                 <button
@@ -811,7 +811,7 @@ const DomainPage: React.FC = () => {
                                   <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 group-hover:scale-110 transition-transform" viewBox="0 0 20 20" fill="currentColor">
                                     <path fillRule="evenodd" d="M11.3 1.046A1 1 0 0112 2v5h4a1 1 0 01.82 1.573l-7 10A1 1 0 018 18v-5H4a1 1 0 01-.82-1.573l7-10a1 1 0 011.12-.38z" clipRule="evenodd" />
                                   </svg>
-                                  <span>Create Web</span>
+                                  <span>Preview</span>
                                 </button>
                             </div>
                           </li>
