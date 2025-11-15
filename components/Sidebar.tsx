7fddb7e9ea807a09cacdafb1d78a963d148c65f2
Alberto Roman <romanvieito@gmail.com>
2025-11-15 09:42:52 -0500

    Update domain icon in sidebar to link icon

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -26,7 +26,7 @@ const sidebarItems: SidebarItem[] = [
     href: '/domain',
     icon: (
       <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9v-9m0-9v9" />
+        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1" />
       </svg>
     ),
   },
